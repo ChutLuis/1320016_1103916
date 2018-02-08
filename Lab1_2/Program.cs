e0cbb732f14631d32a2643d1602001ea63b155bc
Laboratorio de Computación T-204 <t204@laburl>
2018-02-08 07:49:12 -0600

    Aplicando

--- Lab1_2/Program.cs
+++ Lab1_2/Program.cs
@@ -15,11 +15,14 @@ namespace Lab1_2
             Lista.AddLast(new Libro() { ID = 1, Nombre = "El Quijote", Edicion = "Primera" });
             Lista.AddLast(new Libro() { ID = 2, Nombre = "Baldor", Edicion = "Tercera" });
             Lista.AddLast(new Libro() { ID = 3, Nombre = "Sr. Presidente", Edicion = "Segunda" });
+            //Instancia del delegado 
             DelegateComparision delegado = new DelegateComparision(EsPrimeraEdicion);
             Console.WriteLine(delegado(Lista.ElementAt(2)));
             Console.WriteLine(delegado(Lista.First()));
             Console.WriteLine(delegado(Lista.Last()));
-
+            //buscar el libro baldor
+            var result = Lista.Where(libro => libro.Nombre == "Baldor").ToList();
+            var datos = Lista.Where(x => x.ID == 1).ToList();
             Console.ReadKey();
         }
         static bool EsPrimeraEdicion(Libro libro)
@@ -30,6 +33,13 @@ namespace Lab1_2
             }
             else
                 return false;
+        }
+        static Libro EsBaldor(Libro libro)
+        {
+            if (libro.Nombre == "Baldor")
+                return libro;
+            else
+                return null;
         }
     }
     public class Libro
